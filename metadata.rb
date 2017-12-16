a8f1cac01162aa4fc8713b7e48967a017e3a68f1
Jay Flowers <jay.flowers@gmail.com>
2017-12-16 17:31:15 -0500

    commit for version : 1.0.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jay.flowers@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures DNS"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 name             "bind_dns"
 
 depends          "chef_commons"
